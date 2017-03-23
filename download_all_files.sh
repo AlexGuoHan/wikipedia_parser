714efe573d85a6567898aae3d3a963346582bb5d
root <root@ip-172-31-51-1.ec2.internal>
2017-03-23 14:11:17 +0000

    bug fix

--- download_all_files.sh
+++ download_all_files.sh
@@ -1,9 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 #SBATCH -o TEST.out
 #SBATCH --job-name=test_download
 #SBATCH --ntasks=300
 #SBATCH --time=1-0
 
-mkdir all_dump_files
-cd all_dump_files
 wget -i all_dumps_full.txt
