78147180e965ee059279a5511d83fa5a37ffa2c5
root <root@ip-172-31-51-1.ec2.internal>
2017-03-23 01:03:23 +0000

    bug fix

--- submission.sh
+++ submission.sh
@@ -8,4 +8,4 @@ module load python
 
 # download mwxml
 pip3 install -r requirements.txt
-python3 wikipedia_data_parser.py --pageTitleDir page_titles.txt --allDumpTextDir all_dumps.txt --pageTitleDir page_titles.txt
+python3 wikipedia_data_parser.py --pageTitleDir page_titles.txt --allDumpTextDir all_dumps.txt --pageTitleDir page_titles.txt --cpu 3
