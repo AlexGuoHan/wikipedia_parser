0c87ca02cd95fa7a4cbc7ae326a3f321a0e531c0
root <root@ip-172-31-51-1.ec2.internal>
2017-03-25 00:43:52 +0000

    bug fix

--- getJobInfo.sh
+++ getJobInfo.sh
@@ -1,3 +1,22 @@
-jobid=`cat jobid.txt | cut -c21-28`
-scontrol show jobid $jobid
-~
+#! /bin/sh
+
+
+getJobInfos()
+{
+	task=$1
+	jobid=`cat jobid.txt | cut -c21-28`	
+
+	case $task in 
+		now)
+			scontrol show jobid $jobid
+			;;
+		past)
+			sacct -j $jobid
+			;;
+		*) echo 'now or past'
+			;;
+	esac
+}	
+
+getJobInfos $@
+
