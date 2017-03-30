bbf3638e50d049eef193f75fe5e93cdbb2b1e246
root <root@ip-172-31-51-1.ec2.internal>
2017-03-30 10:29:44 +0000

    bug fix

--- submission.sh
+++ submission.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 #SBATCH -o TEST.out
 #SBATCH --job-name=sentiAnalysis
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=1
 #SBATCH --time=1-0
 
-module load keras
+module load python
 pip3 install -r requirements.txt --user
 python3 sentiAnalysis.py --wikiModelDir . --trainDataDir trainData --dataFileList dataFileList.txt --dataFileDir PARSED  --cpu 0
 
