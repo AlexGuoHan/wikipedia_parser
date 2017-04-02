e107a8aacf844fad5dfe456a688f551595192be1
AlexGuoHan <hanhan1@live.unc.edu>
2017-04-02 13:15:23 -0400

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -5,4 +5,5 @@ git clone https://github.com/AlexGuoHan/wikipedia_parser.git
 mv wikipedia_parser/* .
 rm -rf wikipedia_parser
 ls PARSED | grep .tsv > dataFileList.txt
+rm -rf dataFileArray
 sh createDataFileArray.sh
