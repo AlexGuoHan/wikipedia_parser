71e79c23df1cc90e98a06962340c5285ed1f0a55
AlexGuoHan <hanhan1@live.unc.edu>
2017-04-02 13:07:12 -0400

    Create createFailedDataFailedArray.sh

---
+++ createFailedDataFailedArray.sh
@@ -0,0 +1,17 @@
+rm -rf dataFailFileArray
+mkdir dataFailFileArray
+
+# get the list of JobID that went wrong
+sh getJobInfo.sh past | grep -v COMPLETED | grep -v batch | cut -c9-12 | grep -v ID | grep -v \- > tmp_failed_jobid.txt
+
+idx=0
+for id in `cat tmp_failed_jobid.txt`
+do
+    if [ $id -lt 216 ]
+    then
+        cat dataFileArray/dataFileArray$id.txt >> dataFailFileArray/dataFileArray$idx.txt
+    fi
+    idx=$(($idx + 1))
+done
+
+rm tmp_failed_jobid.txt
